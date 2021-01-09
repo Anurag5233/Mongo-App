c803c033a242ad8ee4e8ad301bbb6868f8802056
“[Anurag5233]” <[anurag.singh6280@gmail.com]>
2021-01-09 18:10:48 +0530

    Started project

---
+++ src/main/java/com/example/MongoApp/demoMongoApp/DemoMongoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.MongoApp.demoMongoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMongoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMongoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MongoApp/demoMongoApp/DemoMongoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MongoApp.demoMongoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMongoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
